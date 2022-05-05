77a65ee0b7214a34ac21475f60dde93801497f1f
Kacper Pańkiewicz <kacper.pankiewicz@wp.pl>
2022-05-05 22:50:11 +0200

    added ScrollbarFrame

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
-from tkinter.ttk import Treeview
-
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+from ScrollbarFrame import ScrollbarFrame
+from functools import partial
 
 class MainFrame:
     def __init__(self):
@@ -17,6 +20,7 @@ class MainFrame:
         offers_label = Label(self.root, text="Oferty")
         offers_label.grid(column=0, row=0, sticky=W, padx=5, pady=5)
 
+        # Begining of filter frame
         filter_frame = Frame(self.root)
 
         files_btn = Button(filter_frame, text="Filtruj")
@@ -38,58 +42,17 @@ class MainFrame:
         location.grid(column=5, row=0, sticky=W, padx=5, pady=5)
 
         filter_frame.grid(column=0, row=1, sticky=W, padx=5, pady=5)
+        # End of filter frame
 
-        offers_table = Treeview(self.root)
-        offers_table['columns'] = ('id', 'nazwa_biura', 'typ', 'cena', 'powierzchnia', 'lokalizacja')
-        offers_table.column("#0", width=0, stretch=NO)
-        offers_table.column("id", anchor=CENTER, width=80)
-        offers_table.column("nazwa_biura", anchor=CENTER, width=80)
-        offers_table.column("typ", anchor=CENTER, width=80)
-        offers_table.column("cena", anchor=CENTER, width=80)
-        offers_table.column("powierzchnia", anchor=CENTER, width=80)
-        offers_table.column("lokalizacja", anchor=CENTER, width=80)
-        offers_table.heading("#0", text="", anchor=CENTER)
-        offers_table.heading("id", text="id", anchor=CENTER)
-        offers_table.heading("nazwa_biura", text="nazwa_biura", anchor=CENTER)
-        offers_table.heading("typ", text="typ", anchor=CENTER)
-        offers_table.heading("cena", text="cena", anchor=CENTER)
-        offers_table.heading("powierzchnia", text="powierzchnia", anchor=CENTER)
-        offers_table.heading("lokalizacja", text="lokalizacja", anchor=CENTER)
-        offers_table.insert(parent='', index='end', iid=0, text='',
-                            values=('1', 'Ninja', '101', 'Oklahoma', 'Moore'))
-        offers_table.insert(parent='', index='end', iid=1, text='',
-                            values=('2', 'Ranger', '102', 'Wisconsin', 'Green Bay'))
-        offers_table.insert(parent='', index='end', iid=2, text='',
-                            values=('3', 'Deamon', '103', 'California', 'Placentia'))
+        # Begining of lists todo dodac jako 2 parametr zassane dane z global csv'ki
+        offers_table = create_list(self.root, [])
         offers_table.grid(column=0, row=2, sticky=W, padx=5, pady=5)
 
-        updates_label = Label(self.root, text="Aktualności")
-        updates_label.grid(column=1, row=0, sticky=W, padx=5, pady=5)
-
-        updates_table = Treeview(self.root)
-        updates_table['columns'] = ('id', 'nazwa_biura', 'typ', 'cena', 'powierzchnia', 'lokalizacja')
-        updates_table.column("#0", width=0, stretch=NO)
-        updates_table.column("id", anchor=CENTER, width=80)
-        updates_table.column("nazwa_biura", anchor=CENTER, width=80)
-        updates_table.column("typ", anchor=CENTER, width=80)
-        updates_table.column("cena", anchor=CENTER, width=80)
-        updates_table.column("powierzchnia", anchor=CENTER, width=80)
-        updates_table.column("lokalizacja", anchor=CENTER, width=80)
-        updates_table.heading("#0", text="", anchor=CENTER)
-        updates_table.heading("id", text="id", anchor=CENTER)
-        updates_table.heading("nazwa_biura", text="nazwa_biura", anchor=CENTER)
-        updates_table.heading("typ", text="typ", anchor=CENTER)
-        updates_table.heading("cena", text="cena", anchor=CENTER)
-        updates_table.heading("powierzchnia", text="powierzchnia", anchor=CENTER)
-        updates_table.heading("lokalizacja", text="lokalizacja", anchor=CENTER)
-        updates_table.insert(parent='', index='end', iid=0, text='',
-                             values=('1', 'Ninja', '101', 'Oklahoma', 'Moore'))
-        updates_table.insert(parent='', index='end', iid=1, text='',
-                             values=('2', 'Ranger', '102', 'Wisconsin', 'Green Bay'))
-        updates_table.insert(parent='', index='end', iid=2, text='',
-                             values=('3', 'Deamon', '103', 'California', 'Placentia'))
+        updates_table = create_list(self.root, [])
         updates_table.grid(column=1, row=2, sticky=W, padx=5, pady=5)
+        # End of lists
 
+        # Begining of button frame
         button_frame = Frame(self.root)
 
         generate_btn = Button(button_frame, text="Generuj", width=10)
@@ -108,7 +71,74 @@ class MainFrame:
         option_menu.grid(column=0, row=3, sticky=N, padx=5, pady=5)
 
         button_frame.grid(column=2, row=2, sticky=N, padx=5, pady=5)
+        # End of button frame
 
+# create window with additional estate data
+def initExtraInformationGui(estate):
+    root = Toplevel()
+    root.title("nr oferty")
+
+    # windows only (remove the minimize/maximize button)
+    root.attributes('-toolwindow', True)
+
+    directory = Path.cwd() / "photos"
+    img = directory / estate.zdjecie_glowne
+
+    response = requests.get("https://media-exp1.licdn.com/dms/image/C5603AQHbhjaf38qzVA/profile-displayphoto-shrink_800_800/0/1615299021631?e=1657152000&v=beta&t=jEKYS5YrkiPgQ5OI8X7G4kgu_WclnX5TumntYiqIKPE")
+    imgLoad = Image.open(BytesIO(response.content))
+    imgLoad.thumbnail((528, 528), Image.ANTIALIAS)
+    render = ImageTk.PhotoImage(imgLoad)
+
+    photo = Label(root, image=render)
+    photo.image = render
+    photo.grid(column=0, row=0, columnspan=2, sticky=N)
+
+# create list of lukaszek
+def create_list(container, data):
+    global action_with_arg, action_with_arg
+    sFrame = ScrollbarFrame(container)
+    frame = sFrame.scrolled_frame
+
+    # grid layout for the input frame
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=3)
+    for id in range(6):
+        # render img
+        response = requests.get(
+            "https://media-exp1.licdn.com/dms/image/C5603AQHbhjaf38qzVA/profile-displayphoto-shrink_800_800/0/1615299021631?e=1657152000&v=beta&t=jEKYS5YrkiPgQ5OI8X7G4kgu_WclnX5TumntYiqIKPE")
+        imgLoad = Image.open(BytesIO(response.content))
+        imgLoad.thumbnail((128, 128), Image.ANTIALIAS)
+        render = ImageTk.PhotoImage(imgLoad)
+
+        # associate img with label & alocate in grid
+        photo = Label(frame, image=render)
+        photo.image = render
+        photo.grid(column=0, row=id, columnspan=2, sticky=N)
+
+        Label(frame, text="typ").grid(column=3, row=id, sticky=N)
+        Label(frame, text="nr oferty").grid(column=4, row=id, sticky=N)
+        action_with_arg = partial(initExtraInformationGui, [])  # todo dodac do partiala obiekt/dictionary z ofertą
+        Button(frame, text="Zobacz", width=8, command=action_with_arg).grid(column=5, row=id, sticky=N)
+
+    # for id, estate in enumerate(newEstates):  todo zdecydowac czy  odczytujemy z csv obiektowo czy jako dictionary - tutaj odpowiedz obiektowa
+    #     objEstate = newEstates.get(estate)
+    #     directory = Path.cwd() / "photos"
+    #      response = requests.get(objEstate.zdjecie_glowne)
+    #
+    #      imgLoad = Image.open(BytesIO(response.content))
+    #      imgLoad.thumbnail((128, 128), Image.ANTIALIAS)
+    #      render = ImageTk.PhotoImage(imgLoad)
+    #      # associate img with label & alocate in grid
+    #      photo = Label(frame, image=render)
+    #      photo.image = render
+    #      photo.grid(column=0, row=id, columnspan=2, sticky=N)
+    #     ttk.Label(frame, text=objEstate.typ).grid(column=3, row=id, sticky=tk.N)
+    #     ttk.Label(frame, text=objEstate.nr_oferty).grid(column=4, row=id, sticky=tk.N)
+    #     action_with_arg = partial(create_details_window, objEstate)
+    #     ttk.Button(frame, text="Zobacz", width=8, command=action_with_arg).grid(column=5, row=id, sticky=tk.N)
+    #
+
+    return sFrame
 
 if __name__ == '__main__':
     MainFrame()
