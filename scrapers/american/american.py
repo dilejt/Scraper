73684b047bbb12c8f025b890d7ace9d38497d4e7
LukaszSinica <lukasz.sinica@o2.pl>
2022-05-07 22:32:41 +0200

    loader AmericanHome + fixes

--- scrapers/american/american.py
+++ scrapers/american/american.py
@@ -1,30 +1,34 @@
+import sys
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import date
 import re
 import csv
 from consts import OFFICE_PROPERTY, NEWLINE, WRITING_MODE, DELIMITER, ENCODING, HEADERS
-from scrapers.american.myhelpers import TEMP_ARR, TEMPLATE, LINKS, FIELD_NAMES
+from progressBar import ProgressBar
+from scrapers.american.myhelpers import TEMP_ARR, TEMPLATE, LINKS, FIELD_NAMES, APARTMENT, PLOT, HOUSE, TYPES
 from helpers import getFileName
 
 
 class Searcher:
+    progressBar = None
     today = date.today()
     result = []
+    offers_len = 0
     offers = {
-        'mieszkania': {
+        'mieszkanie': {
             'count': 1,
             'links': []
         },
-        'domy': {
+        'dom': {
             'count': 1,
             'links': []
         },
-        'dzialki': {
+        'dzialka': {
             'count': 1,
             'links': []
         },
-        'lokale': {
+        'lokal': {
             'count': 2,
             'links': []
         },
@@ -56,7 +60,7 @@ class Searcher:
                         temp['pietro'] = re.findall("(.*)/.", self.get_value(values.strong.contents))[0]
                     if re.findall(".*/(.*)", self.get_value(values.strong.contents)):
                         temp['budynek_pietra'] = re.findall(".*/(.*)", self.get_value(values.strong.contents))[0]
-                elif 'dzialki' == key and 'Powierzchnia' == values.strong.previous_sibling.get_text():
+                elif 'dzialka' == key and 'Powierzchnia' == values.strong.previous_sibling.get_text():
                     temp['powierzchnia_dzialki'] = self.get_value(values.strong.contents)
                 elif 'Cena' == values.strong.previous_sibling.get_text():
                     temp['cena'] = ''.join(re.findall("(\d*\d)", self.get_value(values.strong.contents)))
@@ -87,6 +91,7 @@ class Searcher:
         temp['liczba_zdjec'] = count_photos
         temp['data_skanowania'] = self.today.strftime("%d/%m/%Y")
         print(temp)
+        self.progressBar.progress()
         return temp
 
     def countpages(self):
@@ -95,17 +100,22 @@ class Searcher:
             soup = BeautifulSoup(r, "html.parser", from_encoding="iso-8859-1")
             for offer in soup.findAll('ul', class_='pagination pull-right'):
                 pages = offer.findAll('li')
-                self.offers[(link.split('/'))[4]]['count'] = len(pages)
+                self.offers[TYPES[(link.split('/'))[4]]]['count'] = len(pages)
 
     def searchoffers(self):
         for link in LINKS:
-            for i in range(1, self.offers[(link.split('/'))[4]]['count']):
+            for i in range(1, self.offers[TYPES[(link.split('/'))[4]]]['count']):
+                if len(self.offers[TYPES[(link.split('/'))[4]]]['links']) >= 5:
+                    continue
                 r = urllib.request.urlopen(link + '?page=' + str(i))
                 soup = BeautifulSoup(r, "html.parser", from_encoding="iso-8859-1")
                 for offer in soup.findAll('a', href=True):
+                    if len(self.offers[TYPES[(link.split('/'))[4]]]['links']) == 5:
+                        continue
                     if TEMPLATE in offer['href']:
-                        if offer['href'] not in self.offers[(link.split('/'))[4]]['links']:
-                            self.offers[(link.split('/'))[4]]['links'].append(offer['href'])
+                        if offer['href'] not in self.offers[TYPES[(link.split('/'))[4]]]['links']:
+                            self.offers[TYPES[(link.split('/'))[4]]]['links'].append(offer['href'])
+            self.offers_len = self.offers_len + len(self.offers[TYPES[(link.split('/'))[4]]]['links'])
 
     def getoffers(self):
         for key in self.offers.keys():
@@ -117,14 +127,15 @@ class Searcher:
             writer = csv.DictWriter(f, delimiter=DELIMITER, fieldnames=HEADERS)
             writer.writerows(self.result)
 
-    def run(self):
+    def run(self, root):
         self.countpages()
         self.searchoffers()
+        self.progressBar = ProgressBar(root, self.offers_len)
         self.getoffers()
         self.savetofile()
 
 
-def startAmerican():
+def startAmerican(root):
     print('Wykonuje akcje dla biura Łukasza')
     searcher = Searcher()
-    searcher.run()
+    searcher.run(root)
