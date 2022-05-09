8e52ca2d7e9440c1329969770e9cac6adcead01e
Sammael <szymon.doerfer@wp.pl>
2022-05-09 22:12:55 +0200

    correct issues
    usuniete nieuzywane biblioteki i zmienne

--- mainFrameMethods.py
+++ mainFrameMethods.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from django.forms import Textarea
-from matplotlib.pyplot import acorr
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from PIL import Image, ImageTk
@@ -44,7 +42,7 @@ def initExtraInformationGui(estate):
     root.columnconfigure(1, weight=1)
     
     response = requests.get(estate.get('zdjecie_glowne'), verify=False)
-    
+
     imgLoad = Image.open(BytesIO(response.content))
     imgLoad.thumbnail((512, 512), Image.ANTIALIAS)
     render = ImageTk.PhotoImage(imgLoad)
@@ -53,17 +51,14 @@ def initExtraInformationGui(estate):
     photo.image = render
     photo.grid(column=0, row=0, columnspan=2, sticky=N)
     Label(root, text=estate.get("link")).grid(column=0, row=1, columnspan=2, sticky=N)
-    opis = Text(root, height=10, width=50)
-    opis.tag_configure('tag-center', justify='center')
-
-    colHeaders = enumerate(estate)
-    colHeaders = list(colHeaders)
 
     for index, header in enumerate(estate):
         if (estate.get(header) != "-1" and not header in ["opis", "zdjecie_glowne", "zdjecia_linki", "zdjecie_glowne_link", "link"]):
             Label(root, text=header, anchor="w").grid(column=0, row=index + 2, sticky=N)
             Label(root, text=estate.get(header), anchor="w").grid(column=1, row=index + 2, sticky=N)
 
+    opis = Text(root, height=10, width=50)
+    opis.tag_configure('tag-center', justify='center')
     opis.grid(column=0, columnspan=2, row=index + 3, sticky=N)
     opis.insert(END, estate.get("opis"), 'tag-center')
 
